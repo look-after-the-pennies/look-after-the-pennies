df2ae692bd5a76c17c1488feca59eb6cd813e0a1
iSCJT <sean@speckledbanana.com>
2022-09-25 22:54:01 +0100

    Basic tidy of the quasar layout

--- app/src/layouts/AppLayout.vue
+++ app/src/layouts/AppLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="hHh lpR fFf" class="bg-grey-1">
-    <q-header elevated class="bg-white text-grey-8" height-hint="64">
+    <q-header elevated class="bg-primary text-white" height-hint="64">
       <q-toolbar class="GNL__toolbar">
         <q-btn
           flat
@@ -12,29 +12,39 @@
           class="q-mr-sm"
         />
 
-        <q-toolbar-title v-if="$q.screen.gt.xs" shrink class="row items-center no-wrap">
+        <q-toolbar-title
+          v-if="$q.screen.gt.xs"
+          shrink
+          class="row items-center no-wrap"
+        >
           <!-- <img src="https://cdn.quasar.dev/img/layout-gallery/logo-google.svg"> -->
           <span class="q-ml-sm">Look After The Pennies</span>
         </q-toolbar-title>
 
         <q-space />
 
-        <q-input class="GNL__toolbar-input" outlined dense v-model="search" color="bg-grey-7 shadow-1" placeholder="Search for topics, locations & sources">
+        <q-input
+          class="GNL__toolbar-input bg-white"
+          outlined
+          dense
+          v-model="search"
+          color="shadow-1"
+          placeholder="Search for topics, locations & sources"
+        >
           <template v-slot:prepend>
             <q-icon v-if="search === ''" name="search" />
-            <q-icon v-else name="clear" class="cursor-pointer" @click="search = ''" />
+            <q-icon
+              v-else
+              name="clear"
+              class="cursor-pointer"
+              @click="search = ''"
+            />
           </template>
           <template v-slot:append>
-            <q-btn
-              flat
-              dense
-              round
-              aria-label="Menu"
-              icon="arrow_drop_down"
-            >
+            <q-btn flat dense round aria-label="Menu" icon="arrow_drop_down">
               <q-menu anchor="bottom end" self="top end">
                 <div class="q-pa-md" style="width: 400px">
-                  <div class="text-body2 text-grey q-mb-md">
+                  <div class="text-body2 text-white q-mb-md">
                     Narrow your search results
                   </div>
 
@@ -46,9 +56,7 @@
                       <q-input dense v-model="exactPhrase" />
                     </div>
 
-                    <div class="col-3 text-subtitle2 text-grey">
-                      Has words
-                    </div>
+                    <div class="col-3 text-subtitle2 text-grey">Has words</div>
                     <div class="col-9 q-pl-md">
                       <q-input dense v-model="hasWords" />
                     </div>
@@ -60,16 +68,33 @@
                       <q-input dense v-model="excludeWords" />
                     </div>
 
-                    <div class="col-3 text-subtitle2 text-grey">
-                      Website
-                    </div>
+                    <div class="col-3 text-subtitle2 text-grey">Website</div>
                     <div class="col-9 q-pl-md">
                       <q-input dense v-model="byWebsite" />
                     </div>
 
                     <div class="col-12 q-pt-lg row justify-end">
-                      <q-btn flat dense no-caps color="grey-7" size="md" style="min-width: 68px;" label="Search" v-close-popup />
-                      <q-btn flat dense no-caps color="grey-7" size="md" style="min-width: 68px;" @click="onClear" label="Clear" v-close-popup />
+                      <q-btn
+                        flat
+                        dense
+                        no-caps
+                        color="grey-7"
+                        size="md"
+                        style="min-width: 68px"
+                        label="Search"
+                        v-close-popup
+                      />
+                      <q-btn
+                        flat
+                        dense
+                        no-caps
+                        color="grey-7"
+                        size="md"
+                        style="min-width: 68px"
+                        @click="onClear"
+                        label="Clear"
+                        v-close-popup
+                      />
                     </div>
                   </div>
                 </div>
@@ -80,21 +105,24 @@
 
         <q-space />
 
-        <div class="q-gutter-sm row items-center no-wrap">
-          <q-btn v-if="$q.screen.gt.sm" round dense flat color="text-grey-7" icon="apps">
-            <q-tooltip>Google Apps</q-tooltip>
-          </q-btn>
-          <q-btn round dense flat color="grey-8" icon="notifications">
-            <q-badge color="red" text-color="white" floating>
-              2
-            </q-badge>
+        <div class="q-gutter-sm row items-center justify-between no-wrap">
+          <q-btn round dense flat color="white" icon="notifications">
+            <q-badge color="red" text-color="white" floating> 2 </q-badge>
             <q-tooltip>Notifications</q-tooltip>
           </q-btn>
-          <q-btn round flat>
+          <q-btn
+            to="user-account"
+            round
+            dense
+            flat
+            color="white"
+            icon="fa-user"
+            class="q-ml-md"
+          >
             <q-avatar size="26px">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              <!-- <img src="https://cdn.quasar.dev/img/boy-avatar.png" /> -->
             </q-avatar>
-            <q-tooltip>Account</q-tooltip>
+            <q-tooltip>User Account</q-tooltip>
           </q-btn>
         </div>
       </q-toolbar>
@@ -109,7 +137,13 @@
     >
       <q-scroll-area class="fit">
         <q-list padding class="text-grey-8">
-          <q-item class="GNL__drawer-item" v-ripple v-for="link in links1" :key="link.text" clickable>
+          <q-item
+            class="GNL__drawer-item"
+            v-ripple
+            v-for="link in links1"
+            :key="link.text"
+            clickable
+          >
             <q-item-section avatar>
               <q-icon :name="link.icon" />
             </q-item-section>
@@ -120,30 +154,42 @@
 
           <q-separator inset class="q-my-sm" />
 
-          <q-item class="GNL__drawer-item" v-ripple v-for="link in links2" :key="link.text" clickable>
-            <q-item-section avatar>
-              <q-icon :name="link.icon" />
-            </q-item-section>
+          <q-item
+            class="GNL__drawer-item"
+            v-ripple
+            v-for="link in links3"
+            :key="link.text"
+            clickable
+          >
             <q-item-section>
-              <q-item-label>{{ link.text }}</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-separator inset class="q-my-sm" />
-
-          <q-item class="GNL__drawer-item" v-ripple v-for="link in links3" :key="link.text" clickable>
-            <q-item-section>
-              <q-item-label>{{ link.text }} <q-icon v-if="link.icon" :name="link.icon" /></q-item-label>
+              <q-item-label
+                >{{ link.text }} <q-icon v-if="link.icon" :name="link.icon"
+              /></q-item-label>
             </q-item-section>
           </q-item>
 
           <div class="q-mt-md">
             <div class="flex flex-center q-gutter-xs">
-              <a class="GNL__drawer-footer-link" href="javascript:void(0)" aria-label="Privacy">Privacy</a>
+              <a
+                class="GNL__drawer-footer-link"
+                href="javascript:void(0)"
+                aria-label="Privacy"
+                >Privacy</a
+              >
               <span> · </span>
-              <a class="GNL__drawer-footer-link" href="javascript:void(0)" aria-label="Terms">Terms</a>
+              <a
+                class="GNL__drawer-footer-link"
+                href="javascript:void(0)"
+                aria-label="Terms"
+                >Terms</a
+              >
               <span> · </span>
-              <a class="GNL__drawer-footer-link" href="javascript:void(0)" aria-label="About">About Google</a>
+              <a
+                class="GNL__drawer-footer-link"
+                href="javascript:void(0)"
+                aria-label="About"
+                >About Look After The Pennies</a
+              >
             </div>
           </div>
         </q-list>
@@ -156,76 +202,60 @@
   </q-layout>
 </template>
 
-<script>
-import { ref } from 'vue'
-import { fasEarthAmericas, fasFlask } from '@quasar/extras/fontawesome-v6'
-export default {
-  name: 'GoogleNewsLayout',
-  setup () {
-    const leftDrawerOpen = ref(false)
-    const search = ref('')
-    const showAdvanced = ref(false)
-    const showDateOptions = ref(false)
-    const exactPhrase = ref('')
-    const hasWords = ref('')
-    const excludeWords = ref('')
-    const byWebsite = ref('')
-    const byDate = ref('Any time')
-    function onClear () {
-      exactPhrase.value = ''
-      hasWords.value = ''
-      excludeWords.value = ''
-      byWebsite.value = ''
-      byDate.value = 'Any time'
-    }
-    function changeDate (option) {
-      byDate.value = option
-      showDateOptions.value = false
-    }
-    function toggleLeftDrawer () {
-      leftDrawerOpen.value = !leftDrawerOpen.value
-    }
-    return {
-      leftDrawerOpen,
-      search,
-      showAdvanced,
-      showDateOptions,
-      exactPhrase,
-      hasWords,
-      excludeWords,
-      byWebsite,
-      byDate,
-      links1: [
-        { icon: 'web', text: 'Top stories' },
-        { icon: 'person', text: 'For you' },
-        { icon: 'star_border', text: 'Favourites' },
-        { icon: 'search', text: 'Saved searches' }
-      ],
-      links2: [
-        { icon: 'flag', text: 'Canada' },
-        { icon: fasEarthAmericas, text: 'World' },
-        { icon: 'place', text: 'Local' },
-        { icon: 'domain', text: 'Business' },
-        { icon: 'memory', text: 'Technology' },
-        { icon: 'local_movies', text: 'Entertainment' },
-        { icon: 'directions_bike', text: 'Sports' },
-        { icon: fasFlask, text: 'Science' },
-        { icon: 'fitness_center', text: 'Health ' }
-      ],
-      links3: [
-        { icon: '', text: 'Language & region' },
-        { icon: '', text: 'Settings' },
-        { icon: 'open_in_new', text: 'Get the Android app' },
-        { icon: 'open_in_new', text: 'Get the iOS app' },
-        { icon: '', text: 'Send feedback' },
-        { icon: 'open_in_new', text: 'Help' }
-      ],
-      onClear,
-      changeDate,
-      toggleLeftDrawer
-    }
-  }
+<script setup lang="ts">
+import { ref } from 'vue';
+import { fasEarthAmericas, fasFlask } from '@quasar/extras/fontawesome-v6';
+
+const leftDrawerOpen = ref(false);
+const search = ref('');
+const showAdvanced = ref(false);
+const showDateOptions = ref(false);
+const exactPhrase = ref('');
+const hasWords = ref('');
+const excludeWords = ref('');
+const byWebsite = ref('');
+const byDate = ref('Any time');
+function onClear() {
+  exactPhrase.value = '';
+  hasWords.value = '';
+  excludeWords.value = '';
+  byWebsite.value = '';
+  byDate.value = 'Any time';
 }
+function changeDate(option: string) {
+  byDate.value = option;
+  showDateOptions.value = false;
+}
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+
+const links1 = [
+  { icon: 'web', text: 'Dashboard' },
+  { icon: 'person', text: 'My accounts' },
+  { icon: 'star_border', text: 'Favourites' },
+  { icon: 'search', text: 'Saved searches' },
+  { icon: 'flag', text: 'Tasks' },
+];
+// const links2 = [
+
+//   { icon: fasEarthAmericas, text: 'World' },
+//   { icon: 'place', text: 'Local' },
+//   { icon: 'domain', text: 'Business' },
+//   { icon: 'memory', text: 'Technology' },
+//   { icon: 'local_movies', text: 'Entertainment' },
+//   { icon: 'directions_bike', text: 'Sports' },
+//   { icon: fasFlask, text: 'Science' },
+//   { icon: 'fitness_center', text: 'Health ' },
+// ];
+const links3 = [
+  { icon: '', text: 'Language & region' },
+  { icon: '', text: 'Settings' },
+  { icon: 'open_in_new', text: 'Get the Android app' },
+  { icon: 'open_in_new', text: 'Get the iOS app' },
+  { icon: '', text: 'Send feedback' },
+  { icon: 'open_in_new', text: 'Help' },
+];
 </script>
 
 <style lang="sass">
